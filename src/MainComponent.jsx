fd091896a7b8ef21323450e0bcdfc5faa617fa42
Mohana Majumder <sayantan.kundu95@gmail.com>
2021-05-30 15:38:58 +0530

    initial commit

---
+++ src/MainComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MainComponent = () => {
+    return (
+        <div>
+            
+                
+            
+        </div>
+    )
+}
+
+export default MainComponent;
